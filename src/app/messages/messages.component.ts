3934a793cb5b4dd0978c7b61f39b9cd5e4a25f2f
abhishek2x <abhisheksrivastavabbn@gmail.com>
2021-05-22 01:59:21 +0530

    updated styling

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -9,8 +9,8 @@ import { MessageService } from '../message.service';
 export class MessagesComponent implements OnInit {
 
   constructor(public messageService: MessageService) {}
-  
-  ngOnInit(): void {
+
+  ngOnInit() {
   }
 
 }
